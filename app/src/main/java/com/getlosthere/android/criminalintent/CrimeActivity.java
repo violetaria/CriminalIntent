54c58e1ed4f8bb2dbcb32a50fb6fb5afd9568d7b
violetaria <terri.chu@outlook.com>
2015-12-23 11:37:21 -0500

    adding fragment layout plus fixing it so the action bar shows up

--- app/src/main/java/com/getlosthere/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/getlosthere/android/criminalintent/CrimeActivity.java
@@ -1,29 +1,36 @@
 package com.getlosthere.android.criminalintent;
 
+import android.annotation.TargetApi;
+import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.view.MenuItem;
+import android.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 
-public class CrimeActivity extends FragmentActivity {
+public class CrimeActivity extends AppCompatActivity {
+        //FragmentActivity {
 
+    @TargetApi(11)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+//            ActionBar actionBar = getSupportActionBar();
+//            if (actionBar != null) {
+//                actionBar.setTitle(R.string.title_activity_crime);
+//            }
+//        }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        FragmentManager fm = getFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
