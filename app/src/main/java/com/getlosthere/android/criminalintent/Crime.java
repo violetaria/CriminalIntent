25fbc066ed27d3a56ae705bcccac7d47b14348f1
violetaria <terri.chu@outlook.com>
2015-12-23 11:41:24 -0500

    add date and solved fields to crime class

--- app/src/main/java/com/getlosthere/android/criminalintent/Crime.java
+++ app/src/main/java/com/getlosthere/android/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.getlosthere.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,10 +10,29 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime(){
         // Generate unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
     }
 
     public UUID getId() {
