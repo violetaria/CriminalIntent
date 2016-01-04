86165472d6e559c942761b42b853c4dd126cc132
violetaria <terri.chu@outlook.com>
2016-01-04 14:09:10 -0500

    moving to abstract class for the fragment activity

--- app/src/main/java/com/getlosthere/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/getlosthere/android/criminalintent/CrimeActivity.java
@@ -1,36 +1,12 @@
 package com.getlosthere.android.criminalintent;
 
-import android.annotation.TargetApi;
-import android.app.FragmentManager;
-import android.os.Bundle;
 import android.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 
-public class CrimeActivity extends AppCompatActivity {
-        //FragmentActivity {
+public class CrimeActivity extends SingleFragmentActivity {
 
-    @TargetApi(11)
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
-//            ActionBar actionBar = getSupportActionBar();
-//            if (actionBar != null) {
-//                actionBar.setTitle(R.string.title_activity_crime);
-//            }
-//        }
-
-        FragmentManager fm = getFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
-
-        if (fragment == null){
-            fragment = new CrimeFragment();
-            fm.beginTransaction()
-                    .add(R.id.fragmentContainer, fragment)
-                    .commit();
-        }
-    }
+   @Override
+    protected Fragment createFragment() {
+       return new CrimeFragment();
+   }
 
 }
